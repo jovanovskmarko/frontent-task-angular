ed4faa5eb2c25a6242921b4c5c082daef7053982
unknown <marko.jovanovski66@gmail.com>
2023-12-04 10:23:59 +0100

    list images

---
+++ src/app/photos/photos.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { PhotosService } from './photos.service';
+import { Subscription } from 'rxjs';
+import { IPhotos } from './photos';
+
+@Component({
+  selector: 'app-photos',
+  templateUrl: './photos.component.html',
+  styleUrl: './photos.component.css',
+})
+export class PhotosComponent implements OnInit {
+  constructor(private photoService: PhotosService) {}
+
+  subscription: Subscription | undefined;
+  photos: IPhotos[] = [];
+  errorMessage: string = '';
+  title: string = 'Mini Instagram';
+
+  ngOnInit(): void {
+    this.subscription = this.photoService.getProducts().subscribe({
+      next: (photos) => {
+        this.photos = photos;
+      },
+      error: (err) => (this.errorMessage = err),
+    });
+  }
+}
---
+++ src/app/photos/photos.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { IPhotos } from './photos';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PhotosService {
+  constructor(private http: HttpClient) {}
+
+  private apiUrl: string =
+    'https://jsonplaceholder.typicode.com/albums/1/photos';
+
+  getProducts(): Observable<IPhotos[]> {
+    return this.http.get<IPhotos[]>(this.apiUrl).pipe(
+      tap((data) => console.log('All ', JSON.stringify(data))),
+      catchError(this.handleError)
+    );
+  }
+
+  handleError(err: HttpErrorResponse) {
+    let errMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errMessage = `An error occured:  ${err.error.message}`;
+    } else {
+      errMessage = `Server returned code ${err.status}, error message is ${err.error.message}`;
+    }
+    console.log(errMessage);
+    return throwError(() => errMessage);
+  }
+}
